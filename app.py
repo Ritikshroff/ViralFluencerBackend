c8093b1b9690674a1a0249148524307b341a49fe
Ritik Shroff <109219868+Ritikshroff@users.noreply.github.com>
2024-12-31 23:03:34 +0530

    up

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for cross-origin requests (React to Flask)
@@ -28,4 +29,6 @@ def health_check():
     return "Flask backend is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
