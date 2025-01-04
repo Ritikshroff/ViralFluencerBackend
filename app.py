7d013cc41bc2893031283506d31a3d9933973c56
Ritik Shroff <109219868+Ritikshroff@users.noreply.github.com>
2025-01-04 22:27:31 +0530

    change

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ client = MongoClient("mongodb+srv://Ritik:ritik123@cluster0.btkvbre.mongodb.net/
 db = client["contact_db"]  # Replace with your desired database name
 collection = db["contacts"]  # Replace with your desired collection name
 influencers_collection = db["influencers"]  # Collection for influencer signups
+brand_collection = db["brand"]  # Collection for brand signups
+agency_collection = db["agency"]  # Collection for agency signups
+partner_collection = db["partner"]  # Collection for partner signups
 
 # Route to handle form submissions
 @app.route('/submit-form', methods=['POST'])
@@ -31,6 +34,39 @@ def signup_influencer():
         # Insert data into the influencers collection
         influencers_collection.insert_one(influencer_data)
         return jsonify({"message": "Influencer signup successful!"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+# Route to handle Brand signups
+@app.route('/signup/brand', methods=['POST'])
+def signup_brand():
+    try:
+        brand_data = request.json  # Get the JSON data from the request
+        # Insert data into the influencers collection
+        brand_collection.insert_one(brand_data)
+        return jsonify({"message": "Brand signup successful!"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+# Route to handle agency signups
+@app.route('/signup/agency', methods=['POST'])
+def signup_agency():
+    try:
+        agency_data = request.json  # Get the JSON data from the request
+        # Insert data into the influencers collection
+        agency_collection.insert_one(agency_data)
+        return jsonify({"message": "agency signup successful!"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+# Route to handle agency signups
+@app.route('/signup/partner', methods=['POST'])
+def signup_partner():
+    try:
+        partner_data = request.json  # Get the JSON data from the request
+        # Insert data into the influencers collection
+        partner_collection.insert_one(partner_data)
+        return jsonify({"message": "partner signup successful!"}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
